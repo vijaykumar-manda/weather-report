c1ac96b7064708708821ef4547ded4c84d4f407f
vijaykumar-manda <142983113+vijaykumar-manda@users.noreply.github.com>
2023-08-26 20:58:08 +0530

    Update Api.js
    update in API KEY

--- weather-application-frontend/src/API/Api.js
+++ weather-application-frontend/src/API/Api.js
@@ -2,4 +2,4 @@ export const LOCAL_HOST_URL =
   "http://weather-report.us-west-1.elasticbeanstalk.com";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
-export const WEATHER_API_KEY = "56b761cb356487d8a7fd86f443282ba3";
+export const WEATHER_API_KEY = "YOUR_API_KEY";
